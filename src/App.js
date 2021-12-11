ffb8a35e3521f56920137098574ecd91c64d9dcc
vadym-khodak <vaddimart@gmail.com>
2021-12-11 15:37:28 +0200

    check update

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://vadymkhodak.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Vadym's personal site
         </a>
       </header>
     </div>
